e1d0fa9ab11e259bf21b84f5a8c09b233f5996f2
linehoyb <63612725+linehoyb@users.noreply.github.com>
2024-04-24 16:10:03 +0200

    Add electric car script

--- RPi_1/README.txt.txt
+++ RPi_1/README.txt.txt
@@ -1,14 +1,14 @@
-This Raspberry Pi simulates an electric car.
+This Raspberry Pi emulates an electric car.
 
 Its battery will drain or charge depending if it's conneceted to the charging station or not. 
 This is decided from the last received message from the
-MQTT-topic: "station/connection" where 'msg' == true/false.   
+MQTT-topic: "station/connection" where 'msg' == '0' / '1'.   
 
-The battery charges 1% every 500ms.
-The battery drains 1% every 2000ms.
+The battery charges 1% every 1 sec.
+The battery drains 1% every 2 sec.
 
-If someone needs the cars info, then they can get it by publishing 
-to "car/request_info" (the message doesnt matter), 
-the car will then publish its current battery-level to "car/battery".
+If some other node wants the car's info, then they can get it by publishing to "car/request_info" (the message doesn't matter). 
 
-It will then return to either charging or draining depending on the last received message from "station/connection".
+The car will then publish its current battery-level to "car/battery".
+
+In the background it will continue to either charge or drain the battery depending on the last received message received from "station/connection".
---
+++ RPi_1/electric_car.py
@@ -0,0 +1,152 @@
+'''
+This emulates an electric car, mainly the charging and draining of the battery.
+
+Setup in MQTTX:
+* Subscribe to the topic "car/battery/#" to see the current car info.
+
+* Publish to the topic "car/request_info" to get the updated car info
+* Publish to the topic "station/connection" with the msg 1 or 0 to tell the car if it is connected to the
+* charging station or not.
+
+In Mosquitto:
+I'm running localhost as the broker.
+
+Python script:
+* You only need to start the run and then do the rest in MQTTX.
+
+'''
+
+from stmpy import Driver, Machine
+from threading import Thread
+import paho.mqtt.client as mqtt
+import time
+import json
+
+class Car:
+    def update_battery_percentage(self):
+        while True:
+            if(self.charging):
+                self.battery_percentage += 1
+                if(self.battery_percentage > 100): self.battery_percentage = 100 #Cap the percentage at 100%
+                time.sleep(1)
+            else:
+                self.battery_percentage -= 1 
+                if(self.battery_percentage < 0): self.battery_percentage = 0 #Min percentage is 0%
+                time.sleep(3)
+            print("Current charge:", self.battery_percentage)
+            
+    def on_init(self):
+        self.battery_percentage = 100
+        self.charging = False
+        # MQTT client init
+        self.QoS = 2 
+        self.mqtt_client.publish("car/battery", "{}".format(self.battery_percentage), self.QoS)
+        #Thread simulating the car battery
+        self.battery_percentage_thread = Thread(target=self.update_battery_percentage)
+        self.battery_percentage_thread.start()
+        print("Car init done")
+
+    def disconnecting_from_charger(self):
+        self.charging = False
+        print("Not connected, draining battery")
+        
+    def connecting_to_charger(self):
+        self.charging = True
+        print("Connected, charging battery")
+        
+    def on_information_request(self):
+        # Someone is requesting car info through the MQTT broker
+        print("Information requested")
+        self.mqtt_client.publish("car/battery", "{}".format(self.battery_percentage), self.QoS)
+
+# initial transition
+t0 = {
+    "source": "initial", 
+    "target": "DRAINING",
+    "effect": "on_init;"
+    }
+t1 = {
+    "trigger": "info_requested",
+    "source": "DRAINING",
+    "target": "DRAINING",
+    "effect": "on_information_request"
+    }
+t2 = {
+    "trigger": "info_requested",
+    "source": "CHARGING",
+    "target": "CHARGING",
+    "effect": "on_information_request"
+    }
+t3 = {
+    "trigger": "car_connected_to_station",
+    "source": "DRAINING",
+    "target": "CHARGING",
+    "effect": "connecting_to_charger"
+    }
+
+t4 = {
+    "trigger": "car_disconnected_from_station",
+    "source": "CHARGING",
+    "target": "DRAINING",
+    "effect": "disconnecting_from_charger"
+    }
+
+class MQTT_Client_1:
+    
+    def __init__(self):
+        self.previous_time = time.time()
+        self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        
+    def on_connect(self, client, userdata, flags, rc):
+        print("on_connect(): {}".format(mqtt.connack_string(rc)))
+
+    def on_message(self, client, userdata, msg):
+        
+        print("on_message(): topic: {}".format(msg.topic))
+
+        if(format(msg.topic) == "car/request_info"):
+           self.stm_driver.send("info_requested", "stm_car")
+        
+        if(format(msg.topic) == "station/connection"):
+            # Extracting info from the json-message
+            json_payload = json.loads((msg.payload).decode('utf-8'))
+            msg_value = int(json_payload['msg']) #convert to a boolean
+            print(msg_value)
+            if(msg_value == 1):
+               self.stm_driver.send("car_connected_to_station", "stm_car")
+            else:
+               self.stm_driver.send("car_disconnected_from_station", "stm_car")
+        
+        
+        
+    def start(self, broker, port):
+        print("Connecting to {}:{}".format(broker, port))
+        self.client.connect(broker, port)
+
+        self.client.subscribe("station/connection")
+        self.client.subscribe("car/request_info")
+        try:
+            # line below should not have the () after the function!
+            thread = Thread(target=self.client.loop_forever)
+            thread.start()
+        except KeyboardInterrupt:
+            print("Interrupted")
+            self.client.disconnect()
+
+broker, port = "localhost", 1883
+
+car = Car()
+car_machine = Machine(name="stm_car", transitions=[t0, t1, t2, t3, t4], obj=car)
+car.stm = car_machine
+
+driver = Driver()
+driver.add_machine(car_machine)
+
+myclient = MQTT_Client_1()
+car.mqtt_client = myclient.client
+myclient.stm_driver = driver
+
+myclient.start(broker, port)
+driver.start()
