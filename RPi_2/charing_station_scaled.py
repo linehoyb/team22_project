c4f7a0175a483b2f9c158d55ef9f9dc6d8c8d91a
Martin Schau Draugsvoll <madraugsvoll@hotmail.com>
2024-05-05 17:06:38 +0200

    Led and user_id update
    Added user_id to the stations. Also added some fixes to the Sense Hat module

--- RPi_2/charing_station_scaled.py
+++ RPi_2/charing_station_scaled.py
@@ -11,11 +11,12 @@ import logging
 ##DET SOM MANGLER NÅ ER AT DENNE IKKE KAN SENDE MELDINGER FORDI DEN IKKE HAR NOEN MQTT "ATTRIBUTES"
 class Charging_station():
 
-    def __init__(self, name_id, row, mqtt_client):
+    def __init__(self, name_id, row, mqtt_client, sense_hat):
         #Dette er feil, men copilot likte det så det er en fin start
         self.mqtt_client = mqtt_client
         self.charging = False
         self.connection = False
+        self.sense_hat = sense_hat
         self.colors = (0,0,0) #The colours are initially off
         self.update_led()
         self.status = "Free"
@@ -95,12 +96,7 @@ class Charging_station():
 
         #led = green
         self.update_led()
-        """
-        if(self.color = "on"):
-            sense = SenseHat()
-            colour = (0, 255, 0)
-            sense.clear(colour)
-        """
+
         print("The station is free, and is not connected to a car.")
 
     def on_booked(self):
@@ -108,40 +104,25 @@ class Charging_station():
         self.mqtt_client.publish("station/status", json.dumps({"id": self.id, "status": self.status}), self.QoS)        #json.dumps({"id": self.id_number, "status": self.status})
         #led = blue
         self.update_led()
-        """
-        if(self.color = "on"):
-            sense = SenseHat()
-            colour = (0, 0, 255)
-            sense.clear(colour)
-        """
+
         print("The station is booked, but not connected to a car.")
 
-    def on_connected(self):
+    def on_connected(self): #Jeg mangler å legge til user_id til stasjonen
         self.status="Connected"
         self.mqtt_client.publish("station/status", json.dumps({"id": self.id, "status": self.status}), self.QoS)
         #led = yellow
         self.update_led()
-        """
-        if(self.color = "on"):
-            sense = SenseHat()
-            colour = (255, 255, 0)
-            sense.clear(colour)
-        """
+
         #Kan eventuelt adde at det er en fade her og.
         #timer?
         print("The station is connected to a car.")
 
-    def on_disconnected(self):
+    def on_disconnected(self): #Jeg mangler å fjærne user_id fra stasjonen
         self.status="Disconnected"
         self.mqtt_client.publish("station/status", json.dumps({"id": self.id, "status": self.status}), self.QoS)
         #led = red?
         self.update_led()
-        """
-        if(self.color = "on"):
-            sense = SenseHat()
-            colour = (255, 0, 0)
-            sense.clear(colour)
-        """
+
         print("The station is disconnected from a car.")
         #publish station/connection = 0
     
@@ -158,7 +139,7 @@ class Charging_station():
 class Station_Manager():
     #lalala
     
-    def __init__(self):
+    def __init__(self, sense_hat):
         #Starter med å lage en mqtt client:
         self.client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION1)
         #Callback methods:
@@ -172,6 +153,8 @@ class Station_Manager():
         self.client.subscribe("phone/connected")
         #Start the internal loop to process MQTT messages
         self.client.loop_start()
+        self.stations = {}
+        self.sense_hat = sense_hat
 
         #Starter så stmpy driveren:
         self.stm_driver = stmpy.Driver()
@@ -182,7 +165,9 @@ class Station_Manager():
         for row in range(8):
             self.name_id = "station_" + str(row)
             print(self.name_id)
-            new_stm = Charging_station(self.name_id ,row ,self.client)
+            self.stations[row] = self.name_id
+            self.stations[self.name_id] = None
+            new_stm = Charging_station(self.name_id ,row ,self.client, self.sense_hat)
             self.stm_driver.add_machine(new_stm.stm)
             self.stm_driver.start()
 
@@ -205,6 +190,7 @@ class Station_Manager():
         #Neste så må jeg finne ut hva slags command jeg har fått:
         #json.topic = hvor jeg fikk den fra (eks: station/connection)
         json_topic = msg.topic
+
         #json.payload = hva som ble sendt (eks: "msg": 1)
         json_id = json_msg["station_id"]
         
@@ -219,10 +205,15 @@ class Station_Manager():
                 self.stm_driver.send("statoion_free", json_id)
         if(json_topic == "phone/connected"):
             json_payload = json_msg["connecteed"]
-
+            #Trenger å store denne et sted så jeg kan bruke den senere
+            json_user_id = json_msg["user_id"]
             if(json_payload == str(1)):
+                self.stations[json_id] = json_user_id
+                self.mqtt_client.publish("station/connection", json.dumps({"station_id": json_id, "status": 1, "user_id": json_user_id}), self.QoS)
                 self.stm_driver.send("car_connected", json_id)
             else:
+                self.stations[json_id] = None
+                self.mqtt_client.publish("station/connection", json.dumps({"station_id": json_id, "status": 0, "user_id": json_user_id}), self.QoS)
                 self.stm_driver.send("car_disconnected", json_id)
         
 
@@ -242,6 +233,6 @@ class Station_Manager():
 #Prøver selv under her. Fikk ikke Line sitt til å funke (:
 
 #Dette er debugging tools om jeg trenger de til senere
+sense_hat = SenseHat()
 
-
-Manager = Station_Manager()
+Manager = Station_Manager(sense_hat)#Add "sense_hat" to the arguments when needed
